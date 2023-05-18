8a8d7cfc41d6bdfc3c4fd787eaea5d33dce0790a
peyzoman <peyzo627@gmail.com>
2023-05-18 16:33:49 +0330

    Setup basic structure and dependencies

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App) {
+
+}
 
 func main() {
-	fmt.Println("main")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("could not load environment file")
+	}
+
+	app := fiber.New()
+	app.Use(logger.New())
+	setupRoutes(app)
+	app.Listen(":3000")
 }
